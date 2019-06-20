df4d5f2b427c1d1b4e56ed2429c62a1b2e69cea5
zoothezoo <kohekoshien@gmail.com>
2019-06-20 16:58:17 +0900

    listに入れて計算するの成功

--- app/src/main/java/zoothezoo/com/caluculator/ResultActivity.kt
+++ app/src/main/java/zoothezoo/com/caluculator/ResultActivity.kt
@@ -2,11 +2,25 @@ package zoothezoo.com.caluculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+
+        val result = findViewById<TextView>(R.id.resultText)
+        result.text = "0"
+
+        val ans = intent.getStringExtra("ans" )
+
+
+
+        result.text = ans
+
     }
+
 }
